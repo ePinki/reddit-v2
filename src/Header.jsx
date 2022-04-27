e9221f9f07879d695996d740bd831ba860b84361
Nazar Yarmolchik <yrnazarusa@gmail.com>
2022-04-28 00:33:00 +0300

    настройка wepack'a

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 export function Header() {
   return (
     <header>
-      <h1>New Reddit</h1>
+      <h1>New Reddit 123123</h1>
     </header>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === 'development';
+const IS_PROD = NODE_ENV === 'prodaction';
+
+function setupDevtool() {
+    if (IS_DEV) return 'eval';
+    if (IS_PROD) return false;
+}
 
 module.exports = {
     resolve: {
@@ -23,5 +30,12 @@ module.exports = {
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'index.html')
         })
-    ]
+    ],
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: IS_DEV,
+
+    },
+    devtool: setupDevtool(),
 };
